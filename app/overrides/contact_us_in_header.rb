2c2636611fbee8e12ad66a2744a9cad9a72a8b4c
Olivier Buffon <olivierbuffon@gmail.com>
2012-07-20 13:12:48 -0400

    Disable auto-link overrides

--- app/overrides/contact_us_in_header.rb
+++ app/overrides/contact_us_in_header.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "contact_us_in_header",
                      :insert_bottom => "#main-nav-bar",
-                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/contact-us') ? 'current' : 'not'%>'><%= link_to t('spree.contact_us.contacts.new.contact_us'), '/contact-us'  %></li>")
+                     :text => "<li class='<%= (request.fullpath.gsub('//','/') == '/contact-us') ? 'current' : 'not'%>'><%= link_to t('spree.contact_us.contacts.new.contact_us'), '/contact-us'  %></li>",
+                     :disable => true)
